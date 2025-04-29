724bec167781f87bef2cdce7db927ab2060bd962
Sasin Godar <sago_bca2080@lict.edu.np>
2025-04-29 15:30:21 +0545

    binarysearxh

---
+++ BinarySearch.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int binary_search(int arr[], int size, int key) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2; // To prevent overflow
+
+        if (arr[mid] == key) {
+            return mid; // Target found at index mid
+        } else if (arr[mid] < key) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+
+    return -1; // Target not found
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 5;
+    int result = binary_search(arr, size, key);
+
+    if (result != -1) {
+        printf("Element found at index: %d\n", result);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
+}
+//       break;
