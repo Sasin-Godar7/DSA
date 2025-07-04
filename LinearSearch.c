121962c25ed0475e7993dcc28ba5723bd21d20f7
Sasin Godar <sago_bca2080@lict.edu.np>
2025-07-04 12:24:54 +0545

    update

---
+++ LinearSearch.c
@@ -0,0 +1,20 @@
+// to perform the linear searching 
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int result , arr[n],n,key;
+    printf("enter the size of an array :");
+    scanf("%d",n);
+
+    printf("\nenter the elements in an array :");
+    for(int i=0;i<=n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("\n enter the key value to search :");
+    scanf("%d",&key);
+
+}
