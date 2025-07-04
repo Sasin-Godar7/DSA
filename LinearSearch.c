0f99e4643e6bec6fdfdf5d2bd6bee03fdae6edae
Sasin Godar <sago_bca2080@lict.edu.np>
2025-07-04 12:30:28 +0545

    update

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,20 +1,46 @@
-// to perform the linear searching 
+// to perform the linear searching
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    int result , arr[n],n,key;
+    int result, n, key;
     printf("enter the size of an array :");
-    scanf("%d",n);
+    scanf("%d", n);
+    int arr[n];
 
     printf("\nenter the elements in an array :");
-    for(int i=0;i<=n;i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
 
     printf("\n enter the key value to search :");
-    scanf("%d",&key);
+    scanf("%d", &key);
 
+    result = linearSearch(arr, n, key);
+
+    if (result != -1)
+    {
+        printf("\nelement found at the index %d", result);
+    }
+    else
+    {
+        printf("\n sorry the element you searched doesnot found :( ");
+    }
+}
+
+int linearSearch(int arr[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+    }
 }
