ba0593838ff5ba97b963326e09799079abe137d4
Sasin Godar <sago_bca2080@lict.edu.np>
2025-07-04 12:46:30 +0545

    update

---
+++ Bsearch.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+// int binarySearch(int left , int right, int mid, int key );
+
+int main()
+{
+    int n, key;
+    printf("enter the size of an array :");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("\n enter the elements in the array :");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\n enter the key elements to search :");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, 0, n - 1, key);
+}
+
+int binarySearch(int arr[], int left, int right, int key)
+{
+    if (left > right)
+    {
+        return -1;
+    }
+
+    int mid = (left + right) / 2;
+
+    if (arr[mid] == key)
+    {
+        return mid;
+    }
+    else if (key < arr[mid])
+    {
+        return binarySearch(arr, left, mid - 1, key);
+    }
+    else
+    {
+        return binarySearch(arr, mid + 1, right, key);
+    }
+}
